7aef2f6946f5679957888ab14503c31f57cf6dec
ErickD16 <edhiserduran@uade.edu.ar>
2026-03-25 22:26:35 -0600

    Add more gift options and invalid input handling

--- repaso.py
+++ repaso.py
@@ -1,4 +1,10 @@
 #hola
-print("feliz navidad:")
-print("seleccione")
-
+print("feliz navidad")
+print("seleccione un regalo por favor.")
+nume = int(input("Ingrese el número del regalo que desea: "))   
+if nume == 1:
+    print("Usted ha seleccionado un auto nuevo.")
+elif nume == 2:
+    print("Usted ha seleccionado un viaje a la playa.")
+else:
+    print("Número inválido. Por favor seleccione 1 o 2.")                   
